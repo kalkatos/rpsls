442802a77ad89b22a9076ebc6c0ebcf1af7ae193
Alex Kalkatos <alexcsts@gmail.com>
2022-09-05 07:53:18 -0300

    Dotween inclusion & player info spawn in game

--- Rpsls/Assets/Scripts/Runtime/GUI/GameScreen.cs
+++ Rpsls/Assets/Scripts/Runtime/GUI/GameScreen.cs
@@ -1,26 +1,53 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Sirenix.OdinInspector;
+using Kalkatos.Network;
+using DG.Tweening;
 
 namespace Kalkatos.Rpsls
 {
     public class GameScreen : MonoBehaviour
     {
+        [SerializeField, ChildGameObjectsOnly] private Transform playerSlotsListParent;
         [SerializeField, ChildGameObjectsOnly] private Button exitButton;
+
+        private RpslsGameSettings settings;
+        private Dictionary<string, PlayerInfoSlot> playerSlots = new Dictionary<string, PlayerInfoSlot>();
 
         private void Awake ()
         {
+            GameManagerClient.OnPlayerListReceived += HandlePlayerListReceived;
             exitButton.onClick.AddListener(OnExitButtonClicked);
+            settings = RpslsGameSettings.Instance;
         }
 
         private void OnDestroy ()
         {
+            GameManagerClient.OnPlayerListReceived -= HandlePlayerListReceived;
             exitButton.onClick.RemoveListener(OnExitButtonClicked);
+        }
+
+        private void HandlePlayerListReceived (List<PlayerInfo> receivedPlayers)
+        {
+            for (int i = 0; i < receivedPlayers.Count; i++)
+            {
+                PlayerInfoSlot slot = CreateSlot(receivedPlayers[i]);
+                playerSlots.Add(receivedPlayers[i].Id, slot);
+                slot.transform.DOPunchScale(1.2f * Vector3.one, 0.3f, 1);
+            }
         }
 
         private void OnExitButtonClicked ()
         {
             GameManagerClient.ExitRoom();
+        }
+
+        private PlayerInfoSlot CreateSlot (PlayerInfo info)
+        {
+            PlayerInfoSlot newSlot = Instantiate(settings.GameInfoSlotPrefab, playerSlotsListParent);
+            newSlot.HandlePlayerInfo(info);
+            return newSlot;
         }
     }
 }
---
+++ Rpsls/Assets/Scripts/Runtime/GUI/GameScreenAnimation.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kalkatos.Rpsls
+{
+    public class GameScreenAnimation : MonoBehaviour
+    {
+        
+    }
+}
