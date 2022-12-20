26654d5b71040321d75803e7d56d756d0c64c20e
Alex Kalkatos <alexcsts@gmail.com>
2022-12-20 01:00:35 -0300

    INetwork Client interface with the minimum to run a turn-based game

---
+++ Rpsls/Assets/Scripts/Network/INetworkClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Kalkatos.Network
+{
+	public interface INetworkClient
+	{
+		event Action<byte, object> OnEventReceived;
+
+		bool IsConnected { get; protected set; }
+		bool IsInRoom { get; protected set; }
+		PlayerInfo[] Players { get; protected set; }
+		PlayerInfo MyPlayerInfo { get; protected set; }
+		RoomInfo CurrentRoomInfo { get; protected set; }
+
+		void Connect (object parameter, Action<object> onSuccess, Action<object> onError);
+		void Get (byte key, object parameter, Action<object> onSuccess, Action<object> onError);
+		void Post (byte key, object parameter, Action<object> onSuccess, Action<object> onError);
+	}
+}
