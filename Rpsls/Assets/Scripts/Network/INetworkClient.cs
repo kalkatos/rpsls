763c553033749da2246727db8c5066a7e4830fbc
Alex Kalkatos <alexcsts@gmail.com>
2022-12-23 19:38:14 -0300

    New INetworkClient interface

--- Rpsls/Assets/Scripts/Network/INetworkClient.cs
+++ Rpsls/Assets/Scripts/Network/INetworkClient.cs
@@ -9,11 +9,17 @@ namespace Kalkatos.Network
 		bool IsConnected { get; protected set; }
 		bool IsInRoom { get; protected set; }
 		PlayerInfo[] Players { get; protected set; }
-		PlayerInfo MyPlayerInfo { get; protected set; }
-		RoomInfo CurrentRoomInfo { get; protected set; }
+		PlayerInfo MyInfo { get; protected set; }
 
 		void Connect (object parameter, Action<object> onSuccess, Action<object> onError);
 		void Get (byte key, object parameter, Action<object> onSuccess, Action<object> onError);
 		void Post (byte key, object parameter, Action<object> onSuccess, Action<object> onError);
+	}
+
+	public class PlayerInfo
+	{
+		public string PlayerId;
+		public string Nickname;
+		public string[] CustomData;
 	}
 }
