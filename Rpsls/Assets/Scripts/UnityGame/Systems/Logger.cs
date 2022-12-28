12aee9bcc4044584be42d264be5b33c0d0e4a981
Alex Kalkatos <alexcsts@gmail.com>
2022-12-28 12:48:18 -0300

    Menu scene with find match WIP

---
+++ Rpsls/Assets/Scripts/UnityGame/Systems/Logger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Kalkatos.UnityGame.Systems
+{
+	public class Logger
+	{
+		public static void Log (string msg) 
+		{
+			Debug.Log(msg);
+		}
+
+		public static void LogWarning (string msg)
+		{
+			Debug.LogWarning(msg);
+		}
+
+		public static void LogError (string msg)
+		{
+			Debug.LogError(msg);
+		}
+	}
+}
---
+++ Rpsls/Assets/Scripts/UnityGame/Systems/MenuScene.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using Kalkatos.Network.Model;
+using Kalkatos.Network.Unity;
+
+namespace Kalkatos.UnityGame.Systems
+{
+	public class MenuScene : MonoBehaviour
+	{
+		
+
+		private static void FindMatch ()
+		{
+			Logger.Log("Finding match...");
+			NetworkClient.FindMatch(
+				(success) =>
+				{
+					Logger.Log("Success finding match.");
+				},
+				(failure) =>
+				{
+					Logger.Log("Failed to find match.");
+				});
+		}
+	}
+}
