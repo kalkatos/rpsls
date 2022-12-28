7c028550eba953f4ef9e8ac1d5f5775d1785d466
Alex Kalkatos <alexcsts@gmail.com>
2022-12-28 12:48:18 -0300

    Menu scene with find match WIP

--- Rpsls/Assets/Scripts/UnityGame/Systems/Storage.cs
+++ Rpsls/Assets/Scripts/UnityGame/Systems/Storage.cs
@@ -34,22 +34,4 @@ namespace Kalkatos.UnityGame.Systems
 			PlayerPrefs.GetFloat(key, defaultValue);
 		}
 	}
-
-	public class Logger
-	{
-		public static void Log (string msg) 
-		{
-			Debug.Log(msg);
-		}
-
-		public static void LogWarning (string msg)
-		{
-			Debug.LogWarning(msg);
-		}
-
-		public static void LogError (string msg)
-		{
-			Debug.LogError(msg);
-		}
-	}
 }
