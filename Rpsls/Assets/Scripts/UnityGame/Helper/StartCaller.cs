576b26b14ffef5149fccb94099b145f619e458ed
Alex Kalkatos <alexcsts@gmail.com>
2023-01-30 18:38:51 -0300

    Match info & Singleton Scriptable Object & Found Match screen & Moving finding match to own scene

--- Rpsls/Assets/Scripts/UnityGame/Helper/StartCaller.cs
+++ Rpsls/Assets/Scripts/UnityGame/Helper/StartCaller.cs
@@ -1,17 +1,14 @@
 ﻿using UnityEngine;
-using Kalkatos.UnityGame.Signals;
 using UnityEngine.Events;
 
 namespace Kalkatos.UnityGame
 {
 	public class StartCaller : MonoBehaviour
 	{
-		[SerializeField] public Signal onStartSignal;
 		[SerializeField] public UnityEvent onStartEvent;
 
 		private void Start ()
 		{
-			onStartSignal?.Emit();
 			onStartEvent?.Invoke();
 		}
 	}
--- Rpsls/Assets/Scripts/UnityGame/Helper/TimedEvent.cs
+++ Rpsls/Assets/Scripts/UnityGame/Helper/TimedEvent.cs
@@ -1,33 +1,130 @@
 ﻿using UnityEngine;
-using Kalkatos.UnityGame.Signals;
 using UnityEngine.Events;
 using Sirenix.OdinInspector;
+using System;
+using UnityEditor.PackageManager;
 
 namespace Kalkatos.UnityGame
 {
 	public class TimedEvent : MonoBehaviour
 	{
-		[SerializeField] private float time;
-		[SerializeField] private bool loop;
-		[SerializeField, ShowIf(nameof(loop))] private int loopCount;
-		[SerializeField] private Signal timeoutSignal;
-		[SerializeField] private UnityEvent timeoutEvent;
-		[SerializeField, ShowIf(nameof(loop))] private Signal loopEndedSignal;
-		[SerializeField, ShowIf(nameof(loop))] private UnityEvent loopEndedEvent;
+		[SerializeField] private TimedEventBit[] events;
+		[SerializeField] private bool multipleEvents;
+		[SerializeField, ShowIf(nameof(multipleEvents))] private bool loop;
+		[SerializeField, ShowIf(nameof(multipleEvents)), ShowIf(nameof(loop))] private int loopCount;
 
+		[ShowIf(nameof(multipleEvents))] public UnityEvent TimeoutEvent;
+		[ShowIf(nameof(multipleEvents))] public UnityEvent AnyTimeoutEvent;
+		[ShowIf(nameof(multipleEvents)), ShowIf(nameof(loop))] public UnityEvent FinalTimeoutEvent;
+
+		private int currentEvent = 0;
 		private int loopCounter;
-		private Coroutine currentWait;
 
-		public bool Loop { get => loop; set => loop = value; }
+		private void Awake ()
+		{
+			foreach (var ev in events)
+			{
+				ev.SetParent(this);
+				ev.FinalTimeoutEvent.AddListener(HandleEventTimeout);
+			}
+		}
 
-		public static void Create (float time, Signal signal = null, UnityEvent ev = null)
+		public static void Create (float time, UnityEvent ev = null)
 		{
 			GameObject timerGO = new GameObject("Timer");
 			var timer = timerGO.AddComponent<TimedEvent>();
-			timer.time = time;
-			timer.timeoutSignal = signal;
-			timer.timeoutEvent = ev;
-			timer.StartTimer(time);
+			timer.AddTimer(time, ev);
+		}
+
+		public void AddTimer (float time, UnityEvent ev)
+		{
+			TimedEventBit[] newEvents = new TimedEventBit[events.Length + 1];
+			for (int i = 0; i < events.Length; i++)
+				newEvents[i] = events[i];
+			TimedEventBit newBit = new TimedEventBit(this)
+			{
+				time = time,
+				TimeoutEvent = ev
+			};
+			newEvents[newEvents.Length - 1] = newBit;
+			events = newEvents;
+		}
+
+		public void Rewind ()
+		{
+			currentEvent = 0;
+			loopCounter = 0;
+			foreach (var ev in events)
+				ev.Rewind();
+		}
+
+		public void StartTimer ()
+		{
+			events[currentEvent].StartTimer();
+		}
+
+		public void Stop (bool invoke = false)
+		{
+			events[currentEvent].Stop(invoke);
+			Rewind();
+			if (invoke)
+			{
+				TimeoutEvent?.Invoke();
+				if (loop)
+					FinalTimeoutEvent?.Invoke();
+			}
+		}
+
+		private void HandleEventTimeout ()
+		{
+			AnyTimeoutEvent?.Invoke();
+			currentEvent++;
+			if (currentEvent >= events.Length)
+			{
+				currentEvent = 0;
+				TimeoutEvent?.Invoke();
+				if (loop)
+				{
+					if (loopCount == 0 || loopCounter < loopCount)
+					{
+						loopCounter++;
+						events[currentEvent].StartTimer();
+					}
+					else
+						FinalTimeoutEvent?.Invoke();
+				}
+			}
+			else
+				events[currentEvent].StartTimer();
+		}
+
+		
+	}
+
+	[Serializable]
+	public class TimedEventBit
+	{
+		public float time;
+		public bool loop;
+		[ShowIf(nameof(loop))] public int loopCount;
+
+		public UnityEvent TimeoutEvent;
+		[ShowIf(nameof(loop))] public UnityEvent FinalTimeoutEvent;
+
+		private int loopCounter;
+		private Coroutine currentWait;
+		private TimedEvent parent;
+
+		public bool Loop { get => loop; set => loop = value; }
+
+		public TimedEventBit (TimedEvent parent)
+		{
+			SetParent(parent);
+		}
+
+		public void SetParent (TimedEvent timedEvent)
+		{
+			parent = timedEvent;
 		}
 
 		public void Rewind ()
@@ -49,25 +146,23 @@ namespace Kalkatos.UnityGame
 		public void Stop (bool invoke = false)
 		{
 			if (currentWait != null)
-				StopCoroutine(currentWait);
+				parent.StopCoroutine(currentWait);
 			Rewind();
 			if (invoke)
 			{
-				timeoutSignal?.Emit();
-				timeoutEvent?.Invoke();
+				TimeoutEvent?.Invoke();
 			}
 		}
 
 		private void RunTimer (float time)
 		{
 			loopCounter++;
-			currentWait = StartCoroutine(MonoExtensions.WaitCoroutine(time, HandleTimerEnded));
+			currentWait = parent.StartCoroutine(MonoExtensions.WaitCoroutine(time, HandleTimerEnded));
 		}
 
 		private void HandleTimerEnded ()
 		{
-			timeoutSignal?.Emit();
-			timeoutEvent?.Invoke();
+			TimeoutEvent?.Invoke();
 			if (loop)
 			{
 				if (Mathf.Approximately(time, 0))
@@ -76,8 +171,7 @@ namespace Kalkatos.UnityGame
 					RunTimer(time);
 				else
 				{
-					loopEndedSignal?.Emit();
-					loopEndedEvent?.Invoke();
+					FinalTimeoutEvent?.Invoke();
 				}
 			}
 		}
