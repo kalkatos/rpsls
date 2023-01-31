fbee46fceb642951c5bba63284c19c7c42941623
Alex Kalkatos <alexcsts@gmail.com>
2023-01-31 15:18:02 -0300

    Input field updatable

---
+++ Rpsls/Assets/Scripts/UnityGame/Signals/Receivers/TmpInputFieldSignalBinding.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using TMPro;
+using Kalkatos.Network.Unity;
+
+namespace Kalkatos.UnityGame.Signals
+{
+	public class TmpInputFieldSignalBinding : MonoBehaviour
+	{
+		[SerializeField] private SignalString signal;
+		[SerializeField] private TMP_InputField inputFieldComponent;
+
+		private void Reset ()
+		{
+			if (inputFieldComponent == null)
+				inputFieldComponent = GetComponent<TMP_InputField>();
+		}
+
+		private void OnEnable ()
+		{
+			signal.OnSignalEmittedWithParam.AddListener(UpdateText);
+			inputFieldComponent.onEndEdit.AddListener(Submit);
+			UpdateText(signal.LastValue);
+		}
+
+		private void OnDisable ()
+		{
+			signal.OnSignalEmittedWithParam.RemoveListener(UpdateText);
+			inputFieldComponent.onEndEdit.RemoveListener(Submit);
+		}
+
+		private void UpdateText (string text)
+		{
+			inputFieldComponent.SetTextWithoutNotify(text);
+		}
+
+		private void Submit (string text)
+		{
+			signal.EmitWithParam(text);
+			NetworkClient.SetNickname(text);
+		}
+	}
+}
