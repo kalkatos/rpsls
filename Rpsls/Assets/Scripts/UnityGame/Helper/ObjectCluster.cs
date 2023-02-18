11ea5abc4c65936e6273675330e439aa884ea322
Alex Kalkatos <alexcsts@gmail.com>
2023-02-18 01:49:18 -0300

    Change to keep showing time bar until the opponent makes their move

--- Rpsls/Assets/Scripts/UnityGame/Helper/ObjectCluster.cs
+++ Rpsls/Assets/Scripts/UnityGame/Helper/ObjectCluster.cs
@@ -18,7 +18,7 @@ namespace Kalkatos.UnityGame
 			if (useChildren)
 				for (int i = 0; i < transform.childCount; i++)
 					transform.GetChild(i).gameObject.SetActive(b);
-			else
+			if (objects != null)
 				foreach (var item in objects)
 					item.SetActive(b);
 		}
