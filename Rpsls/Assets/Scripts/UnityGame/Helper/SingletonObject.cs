c4b6fb84ae0e43f07fd4bb6c09880a011c49e72d
Alex Kalkatos <alexcsts@gmail.com>
2023-02-22 22:16:49 -0300

    Debugger canvas

---
+++ Rpsls/Assets/Scripts/UnityGame/Helper/SingletonObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Kalkatos.UnityGame
+{
+	public class SingletonObject : MonoBehaviour
+	{
+		public static SingletonObject Instance;
+
+		[SerializeField] private bool useGameObject = true;
+
+		private void Awake ()
+		{
+			if (Instance == null)
+				Instance = this;
+			else if (Instance != this)
+				Destroy(useGameObject ? gameObject : this);
+		}
+	}
+}
---
+++ Rpsls/Assets/Scripts/UnityGame/Test/TestScript.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Kalkatos.UnityGame.Test
+{
+	public class TestScript : MonoBehaviour
+	{
+#if UNITY_EDITOR
+		[MenuItem("Test/Test")]
+		public static void Test ()
+		{
+			string value = "1940-13";
+			if (DateTime.TryParse(value, out DateTime time))
+				Debug.Log("It's a DateTime, and this is it parsed: " + time.ToUniversalTime());
+			else if (float.TryParse(value, out float fValue))
+				Debug.Log("It's a float, and this is it parsed: " + fValue);
+			else
+				Debug.Log("It's nothing of those");
+		}
+#endif
+	}
+}
