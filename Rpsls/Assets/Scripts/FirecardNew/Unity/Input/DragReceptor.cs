7d7c80689c04ff9d586e2e43d4af41275cf54188
Alex Kalkatos <alexcsts@gmail.com>
2023-03-07 02:33:44 -0300

    Canvas Limiter & Fixes in drag and drop & Draggable and Droppable set active status bool

--- Rpsls/Assets/Scripts/FirecardNew/Unity/Input/DragReceptor.cs
+++ Rpsls/Assets/Scripts/FirecardNew/Unity/Input/DragReceptor.cs
@@ -1,3 +1,4 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
@@ -6,10 +7,19 @@ namespace Kalkatos.Firecard.Unity
 {
 	public class DragReceptor : MonoBehaviour, IDropHandler
 	{
-		public UnityEvent<Transform> OnDropReceived;
+		[PropertyOrder(2)] public UnityEvent<Transform> OnDropReceived;
+
+		[PropertyOrder(1), SerializeField] private bool canReceiveDrop = true;
+
+		public void SetDropStatus (bool b)
+		{
+			canReceiveDrop = b;
+		}
 
 		public void OnDrop (PointerEventData eventData)
 		{
+			if (!canReceiveDrop)
+				return;
 			OnDropReceived?.Invoke(eventData.pointerPress?.transform);
 			eventData.pointerPress.SendMessage("ReceiveDrop", this, SendMessageOptions.DontRequireReceiver);
 			Debug.Log($"Drop!");//  LastPress = {eventData.lastPress?.name} , PointerPress = {eventData.pointerPress?.name} , PointerEnter = {eventData.pointerEnter?.name} , PointerClick = {eventData.pointerClick?.name} , RawPointerPress = {eventData.rawPointerPress?.name} , Selected Object = {eventData.selectedObject?.name}");
