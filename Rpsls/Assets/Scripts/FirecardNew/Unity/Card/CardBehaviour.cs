5b3e4f0bcc11e8dcfd0718c6980b70a0b6c94172
Alex Kalkatos <alexcsts@gmail.com>
2023-03-10 13:29:40 -0300

    Drag or click to use (play) card

---
+++ Rpsls/Assets/Scripts/FirecardNew/Unity/Card/CardBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using Kalkatos.UnityGame.Rps;
+using UnityEngine;
+
+namespace Kalkatos.Firecard.Unity
+{
+	public class CardBehaviour : MonoBehaviour
+	{
+		public string CardType;
+
+		public void Use ()
+		{
+			CardsManager.Instance.UseCard(transform);
+		}
+	}
+}
--- Rpsls/Assets/Scripts/UnityGame/Debug/CanvasLimiter.cs
+++ Rpsls/Assets/Scripts/UnityGame/Debug/CanvasLimiter.cs
@@ -8,13 +8,31 @@ namespace Kalkatos.UnityGame
         [SerializeField] private Canvas worldCanvas;
 		[SerializeField] private Vector2 maxAspectRatio;
 
+		private void Start ()
+		{
+			Logger.Log($"Updating Canvas Rect (Start) {Screen.width}x{Screen.height}");
+			UpdateCanvasRect();
+		}
+
+		private void OnRectTransformDimensionsChange ()
+		{
+			Logger.Log($"Updating Canvas Rect (Rect Transform Dimensions Change) {Screen.width}x{Screen.height}");
+			UpdateCanvasRect();
+		}
+
 		private void Update ()
 		{
+			UpdateCanvasRect();
+			if (Application.isPlaying && !Application.isEditor)
+				Destroy(this);
+		}
+
+		private void UpdateCanvasRect ()
+		{
+			
 			RectTransform rectTransform = (RectTransform)worldCanvas.transform;
 			float measuredAspectRatio = (float)Screen.width / Screen.height;
 			rectTransform.sizeDelta = new Vector2(Mathf.Min(rectTransform.sizeDelta.y * measuredAspectRatio, rectTransform.sizeDelta.y * maxAspectRatio.x / maxAspectRatio.y), rectTransform.sizeDelta.y);
-			if (Application.isPlaying && !Application.isEditor)
-				Destroy(this);
 		}
 	}
 }
