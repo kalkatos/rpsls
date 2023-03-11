c363abce07f99d762c09a2a1acb17b46290ac57e
Alex Kalkatos <alexcsts@gmail.com>
2023-03-11 01:40:43 -0300

    Input field binding incorporated to Text Binding & Changes to Sprite Binding to use Value Getters

--- Rpsls/Assets/Scripts/UnityGame/ValueGetter/UnityValueGetters.cs
+++ Rpsls/Assets/Scripts/UnityGame/ValueGetter/UnityValueGetters.cs
@@ -40,4 +40,74 @@ namespace Kalkatos.UnityGame
 			return default;
 		}
 	}
+
+	[Serializable, InlineProperty]
+	public class IntValueGetter : IValueGetter<int>
+	{
+		[HorizontalGroup(15), HideLabel]
+		public ValueType Type;
+		[HorizontalGroup, HideLabel, ShowIf(nameof(Type), ValueType.Simple)]
+		public int SimpleValue;
+		[HorizontalGroup, HideLabel, ShowIf(nameof(Type), ValueType.Random)]
+		public Vector2Int RandomValue;
+		[HorizontalGroup, HideLabel, ShowIf(nameof(Type), ValueType.Scriptable)]
+		public ScriptableObject ScriptableValue;
+
+		public enum ValueType { Simple, Random, Scriptable }
+
+		public int GetValue ()
+		{
+			switch (Type)
+			{
+				case ValueType.Simple:
+					return SimpleValue;
+				case ValueType.Random:
+					return UnityEngine.Random.Range(RandomValue.x, RandomValue.y);
+				case ValueType.Scriptable:
+					if (ScriptableValue != null)
+					{
+						if (ScriptableValue is IValueGetter<int>)
+							return ((IValueGetter<int>)ScriptableValue).GetValue();
+						Logger.LogWarning($"[IntValueGetter] The scriptable {ScriptableValue.name} is not a IValueGetter.");
+					}
+					else
+						Logger.LogWarning($"[IntValueGetter] The scriptable value is null.");
+					break;
+			}
+			return default;
+		}
+	}
+
+	[Serializable, InlineProperty]
+	public class StringValueGetter : IValueGetter<string>
+	{
+		[HorizontalGroup(15), HideLabel]
+		public ValueType Type;
+		[HorizontalGroup, HideLabel, ShowIf(nameof(Type), ValueType.Simple)]
+		public string SimpleValue;
+		[HorizontalGroup, HideLabel, ShowIf(nameof(Type), ValueType.Scriptable)]
+		public ScriptableObject ScriptableValue;
+
+		public enum ValueType { Simple, Scriptable }
+
+		public string GetValue ()
+		{
+			switch (Type)
+			{
+				case ValueType.Simple:
+					return SimpleValue;
+				case ValueType.Scriptable:
+					if (ScriptableValue != null)
+					{
+						if (ScriptableValue is IValueGetter<string>)
+							return ((IValueGetter<string>)ScriptableValue).GetValue();
+						Logger.LogWarning($"[StringValueGetter] The scriptable {ScriptableValue.name} is not a IValueGetter.");
+					}
+					else
+						Logger.LogWarning($"[StringValueGetter] The scriptable value is null.");
+					break;
+			}
+			return default;
+		}
+	}
 }
