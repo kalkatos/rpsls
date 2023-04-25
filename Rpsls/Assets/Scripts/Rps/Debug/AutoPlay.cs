afb4307aa5eec25a42b857a3b61f4338b0c42b48
Alex Kalkatos <alexcsts@gmail.com>
2023-04-25 00:17:43 -0300

    Shortcuts for debug

--- Rpsls/Assets/Scripts/Rps/Debug/AutoPlay.cs
+++ Rpsls/Assets/Scripts/Rps/Debug/AutoPlay.cs
@@ -13,13 +13,24 @@ public class AutoPlay : MonoBehaviour
     {
         if (!isActive)
             return;
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            isActive = false;
+            return;
+        }
         DateTime now = DateTime.UtcNow;
         float seconds = secondsToWait + (float)TimeSpan.FromTicks(secondsToWait * TimeSpan.TicksPerSecond - now.Ticks % (secondsToWait * TimeSpan.TicksPerSecond)).TotalSeconds;
 		Debug.Log($"Waiting {seconds} seconds.");
         StartCoroutine(ClickPlayButton(seconds));
     }
 
-    private IEnumerator ClickPlayButton (float timeToWait)
+	private void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+            playButtonClickSignal.Emit();
+	}
+
+	private IEnumerator ClickPlayButton (float timeToWait)
     {
         yield return new WaitForSeconds(timeToWait);
         playButtonClickSignal.Emit();
