3f9d685b624cfef33d45e2014e60795ed8e851bc
Alex Kalkatos <alexcsts@gmail.com>
2023-05-11 18:06:47 -0300

    Adjustments for hiding urls

---
+++ Rpsls/Assets/Scripts/Rps/ResourceSetter.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Text;
+using UnityEngine;
+
+namespace Kalkatos.UnityGame.Rps
+{
+	[DefaultExecutionOrder(-999)]
+    public class ResourceSetter : MonoBehaviour
+    {
+		private void Awake ()
+		{
+			byte[] bytes = File.ReadAllBytes($"{Application.dataPath}/uris");
+			Storage.Save("Uris", Encoding.UTF8.GetString(bytes));
+		}
+	}
+}
