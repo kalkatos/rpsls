671b43bb166e41ae817c8342b53662a488cc1e11
Alex Kalkatos <alexcsts@gmail.com>
2023-05-27 18:53:57 -0300

    Runtime initializer

---
+++ Rpsls/Assets/Scripts/Rps/RuntimeInitializer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Kalkatos.UnityGame.Rps
+{
+    public class RuntimeStartRunner : MonoBehaviour
+    {
+		[RuntimeInitializeOnLoadMethod]
+		public static void DoRuntimeStartRoutines ()
+		{
+			LoadUrlPrefix();
+		}
+
+		private static void LoadUrlPrefix ()
+		{
+			Logger.Log("Runtime Start Runner");
+			TextAsset configAsset = (TextAsset)Resources.Load("urlprefix");
+			if (configAsset == null)
+			{
+				Logger.LogWarning("UrlPrefix asset not found.");
+				return; 
+			}
+			if (string.IsNullOrEmpty(configAsset.text))
+			{
+				Logger.LogWarning("UrlPrefix asset text is empty.");
+				return; 
+			}
+			Storage.Save("UrlPrefix", configAsset.text);
+		}
+	}
+}
